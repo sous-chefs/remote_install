39322f1c014dcc7939c01e5c5bfb53d9e6997c57
Tim Smith <tsmith84@gmail.com>
2015-09-09 20:30:03 -0700

    Add supermarket metadata and additional platforms

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name              'remote_install'
 maintainer        'Chef Software, Inc.'
-maintainer_email  'releng@getchef.com'
+maintainer_email  'releng@chef.io'
 license           'Apache 2.0'
 description       'Provides an opinionated way to download and install remote software'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -9,7 +9,13 @@ version           '1.0.0'
 supports 'centos'
 supports 'debian'
 supports 'fedora'
+supports 'oracle'
+supports 'redhat'
 supports 'freebsd'
+supports 'scientific'
 supports 'mac_os_x'
 supports 'ubuntu'
 supports 'windows'
+
+source_url 'https://github.com/chef-cookbooks/remote_install' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/remote_install/issues' if respond_to?(:issues_url)

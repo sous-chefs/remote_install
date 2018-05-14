c0cfd9d831d02bcf4f465f56c0ffe8750773988e
Tim Smith <tsmith@chef.io>
2018-05-14 10:15:29 -0700

    Remove ChefSpec matchers
    These are autogenerated now
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/matchers.rb
+++
@@ -1,7 +0,0 @@
-if defined?(ChefSpec)
-  ChefSpec.define_matcher :remote_install
-
-  def install_remote_install(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:remote_install, :install, resource_name)
-  end
-end
